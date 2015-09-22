cd5e655291055f963b81e755d5fcb24c4770fab9
Ubuntu <ubuntu@ip-172-31-11-230.us-west-1.compute.internal>
2015-09-22 22:52:44 +0000

    batch similarity working for sample data and save to databases

--- batch_similarity/compute_similarity.py
+++ batch_similarity/compute_similarity.py
@@ -1,5 +1,5 @@
 '''
-Created on Sep 21, 2015
+Created on Sep 22, 2015
 
 @author: Shanghai
 '''
@@ -14,11 +14,48 @@ import json
 import csv
 import urllib2
 import sys
+import os
+import time
+import hdfs
 
 
 class ComputeSimilarity(object):
     def __init__(self):
         self.hdfs_path = ""
+        self.temp_file_path = ""
+
+    def compute_similarity(self,df):
+        ''' 
+        parse [{g1,[a1,a2,a3]}, {g2,[a1,a3,a4]}, {g4,[a1,a2,a4]}...]
+        into [(g1,a1),(g1,a2),(g1,a3),(g2,a1),(g2,a2),(g2,a4)....]
+        ''' 
+        df_RDD_1 = df.flatMap(lambda x: [(str(x.gene),str(y.strip("u'[ ]"))) for y in (x.artworks).split(",")])  
+
+        ''' 
+        RDD join itself on Gene_id to get artwork pair
+        RDD_1_join output: {gene_id , (artwork1, artwork2)} => {(artwork1, artwork2),1}
+        RDD_1_count output: {(artwork1, artwork2), <count>}
+        RDD_1_map output: (artwork1, artwork2, <count>)
+        '''
+        RDD_1_join = df_RDD_1.join(df_RDD_1).map(lambda x: (x[1],1))
+        RDD_1_count = RDD_1_join.reduceByKey(lambda a, b: a + b)
+        RDD_1_map = RDD_1_count.map(lambda x: (x[0][0],x[0][1],x[1])).filter(lambda x: x[0]!=x[1])
+        RDD_1_save = RDD_1_map.map(lambda x: {"id_1":x[0],"id_2":x[1],"sim_count":str(x[2])})
+
+        ''' save result '''
+        self.save_to_cassandra(RDD_1_save)
+        self.save_to_hdfs(RDD_1_save)
+
+    def save_to_cassandra(self,RDD_1_save):
+        ''' save to Cassandra '''
+        RDD_1_save.saveToCassandra("art_pin_log","artwork_sim")
+
+    def save_to_hdfs(self,RDD_1_save):
+        ''' save to HDFS '''
+        timestamp = time.strftime('%Y%m%d%H%M%S')
+        path = "%s/art_art_sim_%s.json" % (self.temp_file_path,timestamp)
+        RDD_1_save.coalesce(1).saveAsTextFile(path)
+
 
 
 if __name__ == '__main__':
@@ -27,31 +64,11 @@ if __name__ == '__main__':
     sqlContext = SQLContext(sc)
     cf = ComputeSimilarity()
     cf.hdfs_path = "hdfs://ec2-54-183-55-185.us-west-1.compute.amazonaws.com:9000/insight/artsy/gene_info.json"
+    cf.temp_file_path = "hdfs://ec2-54-183-55-185.us-west-1.compute.amazonaws.com:9000/insight/artsy/temp"
     df = sqlContext.read.json(cf.hdfs_path)
+    cf.compute_similarity(df)
 
-
-    ''' 
-    parse [{g1,[a1,a2,a3]}, {g2,[a1,a3,a4]}, {g4,[a1,a2,a4]}...]
-    into [(g1,a1),(g1,a2),(g1,a3),(g2,a1),(g2,a2),(g2,a4)....]
-    ''' 
-    df_RDD_1 = df.flatMap(lambda x: [(str(x.gene),str(y.strip("u'[ ]"))) for y in (x.artworks).split(",")])  
-
-
-    ''' 
-    RDD join itself on Gene_id to get artwork pair
-    RDD_1_join output: {gene_id , (artwork1, artwork2)} => {(artwork1, artwork2),1}
-    RDD_1_count output: {(artwork1, artwork2), <count>}
-    RDD_1_map output: (artwork1, artwork2, <count>)
-    '''
-    RDD_1_join = df_RDD_1.join(df_RDD_1).map(lambda x: (x[1],1))
-    RDD_1_count = RDD_1_join.reduceByKey(lambda a, b: a + b)
-    RDD_1_map = RDD_1_count.map(lambda x: (x[0][0],x[0][1],x[1])).filter(lambda x: x[0]!=x[1])
     
 
-    ''' save to Cassandra '''
-    RDD_1_save = RDD_1_map.map(lambda x: {"id_1":x[0],"id_2":x[1],"sim_count":str(x[2])})
-    RDD_1_save.saveToCassandra("art_pin_log","artwork_sim")
-
-
- 
+    
 
