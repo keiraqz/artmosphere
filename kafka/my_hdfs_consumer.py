89ca74b3c3dd1c807f8f200d30555817e8a3b27b
keiraqz <keira.qzhou@gmail.com>
2015-09-27 23:58:32 +0000

    remove files

--- kafka/my_hdfs_consumer.py
+++ kafka/my_hdfs_consumer.py
@@ -5,23 +5,6 @@ import os
 
 
 class Consumer(object):
-    """Kafka consumer class with functions to consume messages to HDFS.
-    Messages are blocked into 20MB files and transferred to HDFS in two
-    folders, /user/PuppyPlaydate/history/ and /user/PuppyPlaydate/cached.
-    /history/ will contain all immutable files in the case of needing to
-    rebuild the entire batch view. /cached/ will be deleted on a daily basis
-    for the incremental batch job.
-    Attributes:
-        client: string representing IP:port of the kafka broker
-        consumer: Consumer object specifying the client group, and topic
-        temp_file_path: location of the 20MB file to be appended to before
-            transfer to HDFS
-        temp_file: File object opened from temp_file_path
-        topic: String representing the topic on Kafka
-        group: String representing the Kafka consumer group to be associated
-            with
-        block_cnt: integer representing the block count for print statements
-    """
     def __init__(self, addr, group, topic):
         """Initialize Consumer with kafka broker IP, group, and topic."""
         self.client = KafkaClient(addr)
@@ -29,7 +12,7 @@ class Consumer(object):
                                        max_buffer_size=1310720000)
         self.temp_file_path = None
         self.temp_file = None
-        self.hadoop_path = "/insight/pinterest/history"
+        self.hadoop_path = "/insight/artsy/history"
         # self.cached_path = "/insight/pinterest/cache"
         self.topic = topic
         self.group = group
@@ -47,24 +30,20 @@ class Consumer(object):
         timestamp = time.strftime('%Y%m%d%H%M%S')
         
         # open file for writing
-        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,
-                                                         self.topic,
-                                                         self.group,
-                                                         timestamp)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,self.topic,self.group,timestamp)
         self.temp_file = open(self.temp_file_path,"w")
 
         while True:
             try:
                 # get 5 messages at a time, non blocking
-                messages = self.consumer.get_messages(count=5, block=False)
+                messages = self.consumer.get_messages(count=1000, block=False)
                 # OffsetAndMessage(offset=43, message=Message(magic=0,
                 # attributes=0, key=None, value='some message'))
                 for message in messages:
                     self.temp_file.write(message.message.value + "\n")
 
                 # file size > 20MB
-
-                # if self.temp_file.tell() > 20000000:
+                if self.temp_file.tell() > 20000000:
                     self.flush_to_hdfs(output_dir)
 
                 self.consumer.commit()
@@ -75,28 +54,12 @@ class Consumer(object):
 
 
     def flush_to_hdfs(self, output_dir):
-        """Flushes the 20MB file into HDFS.
-        Code template from https://github.com/ajmssc/bitcoin-inspector.git
-        Flushes the file into two folders under
-        hdfs://user/PuppyPlaydate/history and
-        hdfs://user/PuppyPlaydate/cached
-        Args:
-            output_dir: string representing the directory to store the 20MB
-                before transferring to HDFS
-        Returns:
-            None
-        """
+        """Flushes the 20MB file into HDFS."""
         self.temp_file.close()
-
         timestamp = time.strftime('%Y%m%d%H%M%S')
+        hadoop_fullpath = "%s/%s_%s_%s.dat" % (self.hadoop_path, self.group,self.topic, timestamp)
 
-
-        hadoop_fullpath = "%s/%s_%s_%s.dat" % (self.hadoop_path, self.group,
-                                               self.topic, timestamp)
-        # cached_fullpath = "%s/%s_%s_%s.dat" % (self.cached_path, self.group,
-        #                                        self.topic, timestamp)
-        print "Block {}: Flushing data file to HDFS => {}".format(str(self.block_cnt),
-                                                                  hadoop_fullpath)
+        print "Block {}: Flushing data file to HDFS => {}".format(str(self.block_cnt),hadoop_fullpath)
         self.block_cnt += 1
 
         # place blocked messages into history and cached folders on hdfs
@@ -106,18 +69,16 @@ class Consumer(object):
         # os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.temp_file_path,
         #                                                 cached_fullpath))
         os.remove(self.temp_file_path)
-
         timestamp = time.strftime('%Y%m%d%H%M%S')
-
-        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,
-                                                         self.topic,
-                                                         self.group,
-                                                         timestamp)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,self.topic,self.group,timestamp)
         self.temp_file = open(self.temp_file_path, "w")
 
 
 if __name__ == '__main__':
 
     print "\nConsuming messages..."
-    cons = Consumer(addr="ec2-54-183-55-185.us-west-1.compute.amazonaws.com:9092", group="hdfs-01", topic="pinterest")
-    cons.consume_topic("/home/ubuntu/Artmo/kafka/kafka_messages")
+    cons = Consumer(addr="ec2-52-8-247-28.us-west-1.compute.amazonaws.com:9092,ec2-54-183-69-4.us-west-1.compute.amazonaws.com:9092,ec2-52-8-244-245.us-west-1.compute.amazonaws.com:9092,ec2-54-183-55-185.us-west-1.compute.amazonaws.com:9092", group="hdfs", topic="post_activity")
+    cons.consume_topic("/mnt/my-data/kafka/kafka_messages")
+
+
+
