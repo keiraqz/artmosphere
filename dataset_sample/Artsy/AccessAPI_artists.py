493cd3860f4778ef646918bc1cc825078a22b77b
Keira Zhou <keira.qzhou@gmail.com>
2015-09-28 22:58:15 -0400

    make API call to Artsy.net to get artists info

--- dataset_sample/Artsy/AccessAPI_artists.py
+++ dataset_sample/Artsy/AccessAPI_artists.py
@@ -1,7 +1,6 @@
 '''
 Created on Sep 19, 2015
-
-@author: Shanghai
+@author: Keira Zhou
 '''
 
 import os
@@ -11,7 +10,6 @@ import subprocess
 from sys import argv
 from pprint import pprint
 
-# source_file = '/Users/Shanghai/Developer/Java/workspace/GetArtsy/gene_id_list.txt'
 file_id = 0
 source_file = '/home/ubuntu/Insight/dataset/Artsy/artists/artists_%d.txt' % file_id
 target = open(source_file, 'w')
@@ -22,10 +20,8 @@ result = subprocess.check_output(call_path, shell=True)
 json_output = json.loads(result)
 artworks = json_output['_embedded']['artists']
 target.write(json.dumps(artworks))
-
 file_id = file_id + 1
 next_page = json_output['_links']['next']['href']
-
 target.close()
 
 while (next_page != "") :
@@ -41,18 +37,4 @@ while (next_page != "") :
     next_page = json_output['_links']['next']['href']
     target.close()
 
-
-
-
-# def genData():
-#     while True:
-#         with open(source_file) as f:
-#             for line in f:
-#                 # print line
-#                 # producer.send(topic, key, line.rstrip())
-#                 call_path = "curl -v 'https://api.artsy.net/api/genes/%s' -H 'X-Xapp-Token:JvTPWe4WsQO-xqX6Bts49tlrQV0OaoXmXFl8-RBueb0ZMGgKLlvXvxU3xw9a_qDnuGe2_DGF9-lPh9ToJ3s8_7CidpplHcmtsVHknm5uCmgRSKB9gvz3rHO2Riy-CbXv0LJ-FTjgk65xvR7dBsRCs62246kEN1UizlPCHiFCXtimXdjcNoj4oZMw7qUCwhk4UpzCqZQZcaZzbqx6Rd7CYSuqtQ0-9QP_SSFUZFyQ9D4='" % line
-#                 os.system(call_path) 
-#         f.close()
-# 
-# genData()
 
