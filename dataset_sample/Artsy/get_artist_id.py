fe4d3741c29201bbbbc8f9b3f29e105398a1d0ea
Keira Zhou <keira.qzhou@gmail.com>
2015-09-28 22:59:42 -0400

    Update get_artist_id.py

--- dataset_sample/Artsy/get_artist_id.py
+++ dataset_sample/Artsy/get_artist_id.py
@@ -1,7 +1,7 @@
 '''
 Created on Sep 19, 2015
 
-@author: Shanghai
+@author: Keira Zhou
 '''
 
 from pyspark import SparkContext, SparkConf
@@ -19,20 +19,13 @@ import os
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 
-
 path = "hdfs://ec2-54-183-55-185.us-west-1.compute.amazonaws.com:9000/insight/artsy/artists"
 idlist = "/home/ubuntu/Insight-dataset/Artsy/artist_id.txt"
 
-# text_file = spark.textFile(idlist)
 data=[]
-# for dir_entry in os.listdir(path):
-#     dir_entry_path = os.path.join(path, dir_entry)
-#     if os.path.isfile(dir_entry_path):
 artwork_list = sqlContext.read.json(path)
 all_ids = artwork_list.map(lambda x: x.id)
-#         for id in all_ids.collect():
 ids = all_ids.collect()
-
 target = open(idlist, 'w')
 
 for id in ids:
